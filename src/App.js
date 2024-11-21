9b221c55f650ab279287e30e28c7f2ff36c2f56e
jpaksuniemi <jannepasp@gmail.com>
2024-11-21 17:31:51 +0200

    Added form in preparation to test POST on backend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-const App = () => (
-  <p>
-    hello world!
-  </p>
+const Form = () => (
+  <form>
+    <label for="name">Full name:</label>
+    <input type="text" id="name" name="name"></input> <br />
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email"></input>
+  </form>
 )
+
+const Header = ({text}) => (
+  <h1>{text}</h1>
+)
+
+const App = () => {
+  return (
+    <div>
+      <Header text="Book a table"/>
+      <Header text="Please enter your contact information" />
+      <Form />
+      <p>
+        hello world!
+      </p>
+    </div>
+  )
+}
 
 export default App;
