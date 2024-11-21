dc48bb1d9d1544e3b227b4a7d3b82cf281966d9e
jpaksuniemi <jannepasp@gmail.com>
2024-11-21 22:46:55 +0200

    Deleting reservees via a button next to list item

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import { useEffect, useState } from 'react'
 import reserverService from './service/reservers.js'
 
-const Form = () => (
+const Form = () => {
+
+  return (
   <form>
     <label htmlFor="name">Full name:</label>
     <input type="text" id="name" name="name"></input> <br />
     <label htmlFor="email">Email:</label>
     <input type="text" id="email" name="email"></input>
   </form>
-)
+  )
+}
 
 const Header = ({text}) => (
   <h1>{text}</h1>
 )
 
-const ReserverList = ({reservers}) => (
+const ReserverList = ({reservers, deleteHandler}) => (
   <ul>
-    {reservers.map(reserver => <li id='{reserver.id}'>{reserver.name}</li>)}
+    {reservers.map(r =><li key={r.id}>{r.name} <Button onClickHandler={deleteHandler} id={r.id} text={'Cancel'} /> </li>)}
   </ul>
+)
+
+const Button = ({onClickHandler, text, id}) => (
+  <button value={id} onClick={onClickHandler}>
+    {text}
+  </button>
 )
 
 
@@ -33,15 +42,27 @@ const App = () => {
       })
   }, []);
 
+  const deleteReserver = event => {
+    const reserverToDelete = reservers.find(reserver => reserver.id == event.target.value);
+    console.log(reserverToDelete, event.target.value);
+    reserverService
+      .destroy(reserverToDelete.id)
+      .then(deletedReserver => {
+        console.log('deleted, ', deletedReserver);
+        setReservers(reservers.filter(reserver => reserver.id != deletedReserver.id));
+      })
+  }
+
+  const addReservee = event => {
+
+  }
+
   return (
     <div>
       <Header text="Book a table"/>
       <Header text="Please enter your contact information" />
       <Form />
-      <p>
-        hello world!
-      </p>
-      <ReserverList reservers={reservers} />
+      <ReserverList reservers={reservers} deleteHandler={deleteReserver}/>
     </div>
   )
 }
